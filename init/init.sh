d8175586b68b8a37d94c80f0d01f9b5842452e70
braykov <braykov@gmail.com>
2018-10-15 14:43:46 +0300

    Initial commit

---
+++ init/init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export app_name=amp-endava
+
+export KUBECONFIG=./kube-config
+
+# Install the amp stack with helm
+
+echo -e "\nInstalling the amp stack with helm..."
+
+helm install --name $app_name -f amp-helm-params.yaml stable/lamp
+result=$?
+(( result )) && { echo -e "\nHelm failed with $result.\nTry setting a new app_name in the init.sh script.\n\n" ; exit $result ; }
+
+echo -e "\nPlease wait for init to complete! No more than 30 minutes, I promise..."
+
+# Waiting for half an hour is enough
+typeset -i max_time=1800		# 30min x 60sec
+typeset -i elapsed=0
+typeset -i interval=10
+
+while :
+do
+	sleep $interval
+	(( elapsed+=interval ))
+	echo -n "$elapsed..."
+
+	export CHARTIP=$(kubectl get svc ${app_name}-lamp --output=jsonpath={.status.loadBalancer.ingress..ip})
+	[[ -z $CHARTIP ]] || break
+	(( elapsed > max_time )) && { echo -e "\n\nSomething is wrong. Can't wait any more." ; exit 1 ; }
+done
+
+echo -e "\n\nPopulating web content...\n"
+
+# Get pod name (we only have one pod)
+POD=$(kubectl get pods -o=name)
+POD=${POD#pod/}
+
+kubectl cp ../*.php ${POD}:/var/www/html
+
+echo -e "\nFinished isntallation!!!"
+echo -e "\nOpen http://$CHARTIP in the browser :-)\n\n"
---
+++ init/setup-helm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export KUBECONFIG=./kube-config
+
+# Setup helm's tiller with k8s on Google Cloud
+
+echo -n "Creating a service account for tiller..."
+kubectl --namespace kube-system create serviceaccount tiller
+
+echo -n "Creating a role binding for the tiller account..."
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+#kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
+echo -n "Installing tiller on the cluster..."
+helm init --service-account tiller
+
+echo -n "Finished setting helm with tiller!!!"
